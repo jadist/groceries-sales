be3414e480b71ed1dd73412fcac1603e184e083c
Eldest Pasirula <aknutman@gmail.com>
2022-12-20 22:53:06 +0700

    SalesMasterProduct

---
+++ src/api/sales-master-product/routes/sales-master-product.ts
@@ -0,0 +1,7 @@
+/**
+ * sales-master-product router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sales-master-product.sales-master-product');
