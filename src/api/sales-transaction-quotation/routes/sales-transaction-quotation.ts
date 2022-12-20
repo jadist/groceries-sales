218b662ca753a19c9345fc3a1aa1898d4309402e
Eldest Pasirula <aknutman@gmail.com>
2022-12-21 00:09:27 +0700

    SalesTransactionQuotation

---
+++ src/api/sales-transaction-quotation/routes/sales-transaction-quotation.ts
@@ -0,0 +1,7 @@
+/**
+ * sales-transaction-quotation router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sales-transaction-quotation.sales-transaction-quotation');
---
+++ src/api/sales-transaction-quotation/services/sales-transaction-quotation.ts
@@ -0,0 +1,7 @@
+/**
+ * sales-transaction-quotation service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sales-transaction-quotation.sales-transaction-quotation');
