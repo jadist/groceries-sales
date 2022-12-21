e418c5e7e38548c93927b4613dde58e0c6747303
Eldest Pasirula <aknutman@gmail.com>
2022-12-22 00:01:19 +0700

    SalesTransactionInvoice

---
+++ src/api/sales-transaction-invoice/services/sales-transaction-invoice.ts
@@ -0,0 +1,7 @@
+/**
+ * sales-transaction-invoice service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sales-transaction-invoice.sales-transaction-invoice');
