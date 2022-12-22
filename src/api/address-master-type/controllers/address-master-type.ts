5f288a0aa35f7b4ed22ffbae2903c26c3d707ae8
Eldest Pasirula <aknutman@gmail.com>
2022-12-22 23:26:21 +0700

    AddressMasterType

---
+++ src/api/address-master-type/controllers/address-master-type.ts
@@ -0,0 +1,7 @@
+/**
+ * address-master-type controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::address-master-type.address-master-type');
