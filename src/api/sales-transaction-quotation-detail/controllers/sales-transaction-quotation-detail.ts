05fcae4db429ac331d72eeda3643971d8305c557
Eldest Pasirula <aknutman@gmail.com>
2022-12-27 20:50:23 +0700

    SalesTransactionQuotationDetail

---
+++ src/api/sales-transaction-quotation-detail/controllers/sales-transaction-quotation-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * sales-transaction-quotation-detail controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sales-transaction-quotation-detail.sales-transaction-quotation-detail');
