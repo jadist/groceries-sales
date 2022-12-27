e228664090304cce5bbddd46f9baf959dbb38fdc
Eldest Pasirula <aknutman@gmail.com>
2022-12-27 12:48:59 +0700

    InvoiceMasterTransmissionMethod

---
+++ src/api/invoice-master-transmission-method/controllers/invoice-master-transmission-method.ts
@@ -0,0 +1,7 @@
+/**
+ * invoice-master-transmission-method controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::invoice-master-transmission-method.invoice-master-transmission-method');
