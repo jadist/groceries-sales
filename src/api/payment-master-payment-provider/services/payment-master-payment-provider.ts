880f1b974f772ba0ba7df51970410dd95b822140
Eldest Pasirula <aknutman@gmail.com>
2022-12-27 22:29:09 +0700

    PaymentMasterPaymentProvider

---
+++ src/api/payment-master-payment-provider/services/payment-master-payment-provider.ts
@@ -0,0 +1,7 @@
+/**
+ * payment-master-payment-provider service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::payment-master-payment-provider.payment-master-payment-provider');
