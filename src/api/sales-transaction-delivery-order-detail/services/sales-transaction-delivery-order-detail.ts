fee4d944f9e97dadcab2d199d34814f69bfb428f
Eldest Pasirula <aknutman@gmail.com>
2022-12-27 22:17:05 +0700

    SalesTransactionDeliveryOrderDetail

---
+++ src/api/sales-transaction-delivery-order-detail/services/sales-transaction-delivery-order-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * sales-transaction-delivery-order-detail service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sales-transaction-delivery-order-detail.sales-transaction-delivery-order-detail');
