3152ee0ac3c554ccf541ef521df20bb5da8ed315
Eldest Pasirula <aknutman@gmail.com>
2022-12-27 22:26:28 +0700

    PaymentMasterPaymentMethod

---
+++ src/api/payment-master-payment-method/controllers/payment-master-payment-method.ts
@@ -0,0 +1,7 @@
+/**
+ * payment-master-payment-method controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::payment-master-payment-method.payment-master-payment-method');
---
+++ src/api/payment-master-payment-method/routes/payment-master-payment-method.ts
@@ -0,0 +1,7 @@
+/**
+ * payment-master-payment-method router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::payment-master-payment-method.payment-master-payment-method');
