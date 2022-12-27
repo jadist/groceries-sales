ff41425c1fe03aface885645263c5bde8a9fd69f
Eldest Pasirula <aknutman@gmail.com>
2022-12-27 13:28:12 +0700

    SalesTransactionSale

---
+++ src/api/sales-transaction-sale/services/sales-transaction-sale.ts
@@ -0,0 +1,7 @@
+/**
+ * sales-transaction-sale service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sales-transaction-sale.sales-transaction-sale');
